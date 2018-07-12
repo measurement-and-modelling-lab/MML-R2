d7b8bc757059f8b9581693d3ea97e097200acf58
paulserafini <pserafin@sfu.ca>
2018-07-12 10:05:45 -0700

    Add header bar

--- ui.r
+++ ui.r
@@ -1,6 +1,35 @@
 library(shinythemes)
 
 shinyUI(fluidPage(theme = "simplex.css",
+                  
+                  
+                  HTML('<br>
+  
+    <link rel="stylesheet" type="text/css" href="index.css">
+  <style>
+    html {
+       overflow-y: scroll;
+       }
+       </style>
+    <title>Analytics^2 - About</title>
+         <div class="bar">
+    <b class="title">Measurement and Modelling Lab &nbsp; - &nbsp; Tools</b><br class="rwd-break"><b class="link">
+    <a href="https://shiny.rcg.sfu.ca/u/pserafin/rsquared/"><font color="#00ca8a">R SQUARED</font></a>
+    &emsp;&nbsp;<a href="https://shiny.rcg.sfu.ca/u/pserafin/wbcorr/"><font color="white">WBCORR</font></a>
+    &emsp;&nbsp;<a href="https://shiny.rcg.sfu.ca/u/pserafin/csvgenerator/"><font color="white">CSVGenerator</font></a>
+
+
+
+
+
+        </b>
+        </div>
+         
+         
+         
+         '),
+                  
+                  HTML("<br>"),
 
   tags$head(
     tags$style(HTML("
@@ -49,6 +78,16 @@ shinyUI(fluidPage(theme = "simplex.css",
 	htmlOutput("r2Output")
   )),
 
-  HTML('<br>')
+  HTML('<br>'),
+  HTML('
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <div class="bar2">
+     <b class="bottom">
+     <font color="#717171">Provided by the</font>
+     <a href="http://members.psyc.sfu.ca/labs/mml"><font color=white>Measurement and Modelling Lab</font></a>
+     <font color="#717171"> at</font>
+     <a href="https://www.sfu.ca/"><font color=white> SFU</font></a>
+     </b>
+     </div><br>')
 )
 )
